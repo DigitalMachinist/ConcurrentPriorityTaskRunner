16e7ad0e62b8cbebeea5ed36daf3d09d944ec328
Jeff Rose <jrose0@gmail.com>
2014-12-29 15:31:47 -0500

    Initial commit of the VS2013 project, Doxygen files and NUnit project.

---
+++ ConcurrentPriorityTaskRunner/ConcurrentPriorityTaskRunner.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Axon.Collections;
+
+namespace Axon.Utilities
+{
+    public class ConcurrentPriorityTaskRunner
+    {
+
+    }
+}
---
+++ ConcurrentPriorityTaskRunnerTest/ConcurrentPriorityTaskRunnerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ConcurrentPriorityTaskRunnerTest
+{
+	[TestFixture]
+	public class ConcurrentPriorityTaskRunnerTest
+	{
+		[Test]
+		public void TestMethod1()
+		{
+		}
+	}
+}
